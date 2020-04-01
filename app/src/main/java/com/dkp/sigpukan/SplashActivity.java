2588a1fed0c70d387bda5bf525a30930b3b41b88
pitong10 <pitongku@gmail.com>
2020-04-01 11:12:35 +0700

    Initial commit

---
+++ app/src/main/java/com/dkp/sigpukan/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.dkp.sigpukan;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },3000);
+    }
+}
